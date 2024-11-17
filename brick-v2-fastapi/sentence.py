2ffb11643f5e839a6389ee1db0c36ba0652212ad
brentably <brentably@gmail.com>
2024-11-16 18:52:12 -0800

    increase pipeline speed by fixing caching

--- brick-v2-fastapi/sentence.py
+++ brick-v2-fastapi/sentence.py
@@ -13,6 +13,19 @@ image = (
 )
 volume = modal.Volume.from_name("llamas", create_if_missing=True)
 
+def print_file_structure(directory):
+    for root, dirs, files in os.walk(directory):
+        level = root.replace(directory, '').count(os.sep)
+        if level > 2:
+            continue
+        indent = ' ' * 4 * (level)
+        print(f'{indent}{os.path.basename(root)}/')
+        subindent = ' ' * 4 * (level + 1)
+        for f in files + dirs:
+            print(f'{subindent}{f}')
+            
+model_dir = "/volume/model"
+
 
 @app.function(image=image, secrets=[modal.Secret.from_name("huggingface-secret")], timeout=3600, volumes={"/volume": volume})
 def download_model():
@@ -24,11 +37,18 @@ def download_model():
     # Ensure the module is available before importing
     import huggingface_hub
     from huggingface_hub import snapshot_download
-    snapshot_download(repo_id="meta-llama/Llama-3.2-3B-Instruct",
-                            #   local_dir=volume + "/" + "llama3.2",
-                              token=os.environ["HF_TOKEN"],
-                                ignore_patterns=["*.pt", "*.gguf"],  # Using safetensors
-                              )
+    
+    if not os.path.exists(model_dir):
+        print('downloading')
+        snapshot_download(repo_id="meta-llama/Llama-3.2-3B-Instruct",
+                          token=os.environ["HF_TOKEN"],
+                          ignore_patterns=["*.pt", "*.gguf"],  # Using safetensors
+                          local_dir=model_dir
+                          )
+    else:
+        print('not need download')
+    
+    
     volume.commit()
     
     
@@ -37,6 +57,10 @@ GPU_CONFIG = modal.gpu.H100(count=1)
 @app.function(image=image, secrets=[modal.Secret.from_name("huggingface-secret")], volumes={"/volume": volume}, gpu=GPU_CONFIG)
 def generate(prompt):
     volume.reload()
+    
+    if not os.path.exists(model_dir):
+        print('error')
+
     # Use a pipeline as a high-level helper
     from transformers import pipeline
 
@@ -44,8 +68,19 @@ def generate(prompt):
         {"role": "user", "content": prompt},
     ]
     
-    pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-3B-Instruct", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200)
+    print_file_structure('/volume')
+    
+    if not os.path.exists('/volume/model'):
+        pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-3B-Instruct", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200)
+        pipe.save_pretrained('/volume')
+    else:
+        pipe = pipeline("text-generation", model="/volume/model", tokenizer="/volume", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200)
+
+
+    volume.commit()
+    
     response = pipe(messages)
+    
     return response
 
 
