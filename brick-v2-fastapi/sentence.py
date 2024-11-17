4ca6e6271d80f7c2587e9853abd386f04897c005
brentably <brentably@gmail.com>
2024-11-16 20:17:59 -0800

    struggled with cfg

--- brick-v2-fastapi/sentence.py
+++ brick-v2-fastapi/sentence.py
@@ -2,14 +2,20 @@ import modal
 import subprocess
 import os
 import torch
-
+from transformers import AutoTokenizer
+from transformers_cfg.grammar_utils import IncrementalGrammarConstraint
+from transformers_cfg.generation.logits_process import GrammarConstrainedLogitsProcessor
+from transformers import PreTrainedTokenizer
+from transformers import PreTrainedTokenizerFast
 app = modal.App("sentence-generator")
 
 image = (
     modal.Image.debian_slim(python_version="3.10")
     .apt_install("git")
-    .pip_install(["torch==2.2.1", "huggingface_hub", "hf-transfer", "transformers", "vllm", "hf-transfer==0.1.4", "accelerate>=0.26.0"])
+    .pip_install(["torch==2.2.1", "huggingface_hub", "hf-transfer", "transformers", "vllm", "hf-transfer==0.1.4", "accelerate>=0.26.0", "transformers-cfg"])
     .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+    .copy_local_file('./experimental/top_2000_german.ebnf', '/experimental/top_2000_german.ebnf')
+    .copy_local_file('./experimental/chess.ebnf', '/experimental/chess.ebnf')
 )
 volume = modal.Volume.from_name("llamas", create_if_missing=True)
 
@@ -24,7 +30,7 @@ def print_file_structure(directory):
         for f in files + dirs:
             print(f'{subindent}{f}')
             
-model_dir = "/volume/model"
+model_dir = "/volume/Llama-3.2-3B-Instruct"
 
 
 @app.function(image=image, secrets=[modal.Secret.from_name("huggingface-secret")], timeout=3600, volumes={"/volume": volume})
@@ -70,12 +76,19 @@ def generate(prompt):
     
     print_file_structure('/volume')
     
-    if not os.path.exists('/volume/model'):
+    if not os.path.exists(model_dir):
         pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-3B-Instruct", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200)
         pipe.save_pretrained('/volume')
+        print('save pretrained')
+        print_file_structure('/volume')
     else:
-        pipe = pipeline("text-generation", model="/volume/model", tokenizer="/volume", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200)
-
+        pipe = pipeline("text-generation", model=model_dir, tokenizer=model_dir, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200, use_fast=False)
+    
+    with open("/experimental/chess.ebnf", "r") as file:
+        grammar_str = file.read()
+    # tokenizer = AutoTokenizer.from_pretrained('/volume/model', use_fast=False)
+    grammar = IncrementalGrammarConstraint(grammar_str, "root", tokenizer=pipe.tokenizer)
+    logits_processor = GrammarConstrainedLogitsProcessor(grammar)
 
     volume.commit()
     
@@ -87,4 +100,4 @@ def generate(prompt):
 @app.local_entrypoint()
 def main():
     download_model.remote()
-    print(generate.remote("hey"))
+    print(generate.remote("hey give me a chess move"))
