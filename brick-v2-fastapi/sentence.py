35cf68f31d6e7cd1827d4a22b2acee3fe415fed6
brentably <brentably@gmail.com>
2024-11-17 15:36:08 -0800

    got model inferencing working w/ basic grammar trying to get gpu inferencing working

--- brick-v2-fastapi/sentence.py
+++ brick-v2-fastapi/sentence.py
@@ -1,22 +1,21 @@
 import modal
 import subprocess
 import os
-import torch
-from transformers import AutoTokenizer
-from transformers_cfg.grammar_utils import IncrementalGrammarConstraint
-from transformers_cfg.generation.logits_process import GrammarConstrainedLogitsProcessor
-from transformers import PreTrainedTokenizer
-from transformers import PreTrainedTokenizerFast
+
+
 app = modal.App("sentence-generator")
 
 image = (
-    modal.Image.debian_slim(python_version="3.10")
-    .apt_install("git")
-    .pip_install(["torch==2.2.1", "huggingface_hub", "hf-transfer", "transformers", "vllm", "hf-transfer==0.1.4", "accelerate>=0.26.0", "transformers-cfg"])
-    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
-    .copy_local_file('./experimental/top_2000_german.ebnf', '/experimental/top_2000_german.ebnf')
-    .copy_local_file('./experimental/chess.ebnf', '/experimental/chess.ebnf')
+    modal.Image.from_registry("ghcr.io/ggerganov/llama.cpp:light-cuda", add_python="3.11")
+    .entrypoint([])
+    # .run_commands("ln -sf /usr/bin/python3 /usr/bin/python", "python -V")
+    # .run_commands("echo \"alias python='python3'\" >> /root/.bashrc", "python -V")
+    .copy_local_file('./experimental/top_2000_german.gbnf', '/experimental/top_2000_german.gbnf')
+    .copy_local_file('./experimental/top_600_german.gbnf', '/experimental/top_600_german.gbnf')
+    .copy_local_file('./experimental/chess.gbnf', '/experimental/chess.gbnf')
+
 )
+
 volume = modal.Volume.from_name("llamas", create_if_missing=True)
 
 def print_file_structure(directory):
@@ -30,74 +29,56 @@ def print_file_structure(directory):
         for f in files + dirs:
             print(f'{subindent}{f}')
             
-model_dir = "/volume/Llama-3.2-3B-Instruct"
+model_path = "/volume/Llama-3.2-3B-Instruct-f16.gguf"
 
 
 @app.function(image=image, secrets=[modal.Secret.from_name("huggingface-secret")], timeout=3600, volumes={"/volume": volume})
 def download_model():
     volume.reload()
-
-    subprocess.run(["huggingface-cli", "login", "--token", os.environ["HF_TOKEN"]])
-    
-    
-    # Ensure the module is available before importing
-    import huggingface_hub
-    from huggingface_hub import snapshot_download
-    
-    if not os.path.exists(model_dir):
-        print('downloading')
-        snapshot_download(repo_id="meta-llama/Llama-3.2-3B-Instruct",
-                          token=os.environ["HF_TOKEN"],
-                          ignore_patterns=["*.pt", "*.gguf"],  # Using safetensors
-                          local_dir=model_dir
-                          )
+    if not os.path.exists(model_path):
+        print("Downloading model...")
+        subprocess.run([
+            "curl",
+            "-L",
+            "-o", model_path,
+            "https://huggingface.co/bartowski/Llama-3.2-3B-Instruct-GGUF/resolve/main/Llama-3.2-3B-Instruct-f16.gguf"
+        ], check=True)
+        print("Download complete")
     else:
-        print('not need download')
-    
+        print("Model already exists")
     
     volume.commit()
-    
-    
+
 GPU_CONFIG = modal.gpu.H100(count=1)
 
 @app.function(image=image, secrets=[modal.Secret.from_name("huggingface-secret")], volumes={"/volume": volume}, gpu=GPU_CONFIG)
 def generate(prompt):
     volume.reload()
     
-    if not os.path.exists(model_dir):
-        print('error')
+    if not os.path.exists(model_path):
+        print('Model not found')
+        return
+    
+    # First generate with llama.cpp
+    llama_response = subprocess.run(
+        ["/llama-cli",
+        "-m", model_path, 
+        "-p", prompt, 
+        "-n", "100", 
+        "--ctx-size", "100000",  
+        "--grammar-file", "/experimental/top_600_german.gbnf",
+        "--gpus", "all",
+        "--n-gpu-layers", "1", '-v'], capture_output=True, text=True)
 
-    # Use a pipeline as a high-level helper
-    from transformers import pipeline
+    print('response')
+    print(llama_response.stdout)
+    print('stdrr')
+    print(llama_response.stderr)
 
-    messages = [
-        {"role": "user", "content": prompt},
-    ]
     
-    print_file_structure('/volume')
-    
-    if not os.path.exists(model_dir):
-        pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-3B-Instruct", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200)
-        pipe.save_pretrained('/volume')
-        print('save pretrained')
-        print_file_structure('/volume')
-    else:
-        pipe = pipeline("text-generation", model=model_dir, tokenizer=model_dir, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.environ["HF_TOKEN"], max_new_tokens=200, use_fast=False)
-    
-    with open("/experimental/chess.ebnf", "r") as file:
-        grammar_str = file.read()
-    # tokenizer = AutoTokenizer.from_pretrained('/volume/model', use_fast=False)
-    grammar = IncrementalGrammarConstraint(grammar_str, "root", tokenizer=pipe.tokenizer)
-    logits_processor = GrammarConstrainedLogitsProcessor(grammar)
-
-    volume.commit()
-    
-    response = pipe(messages)
-    
-    return response
-
+    return llama_response
 
 @app.local_entrypoint()
 def main():
     download_model.remote()
-    print(generate.remote("hey give me a chess move"))
+    print(generate.remote("A basic sentence in german: "))
